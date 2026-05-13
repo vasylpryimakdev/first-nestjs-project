bd7b8814237e43ae3b29ce7e69ee8d2134bd8aac
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-13 15:45:28 +0300

    feat(tasks): introduce task model and improve service layer with typed logic

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import type { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  public findAll(): string[] {
-    return ['A', 'B'];
+  public findAll(): ITask[] {
+    return this.tasksService.findAll();
   }
 
   @Get('/:id')
-  public findOne(@Param('id') id: string): string {
-    return `The number is ${id}`;
+  public findOne(@Param('id') id: string): ITask {
+    const task = this.tasksService.findOne(id);
+
+    if (task) {
+      return task;
+    }
+
+    throw new NotFoundException();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ITask } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: ITask[] = [];
+
+  findAll(): ITask[] {
+    return this.tasks;
+  }
+
+  findOne(id: string): ITask | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
 }
