ba7e6c995455831af8a3932f90a8da29523e067a
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-13 16:46:36 +0300

    feat(tasks): add param validation and task status update endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,11 +4,14 @@ import {
   Get,
   NotFoundException,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import type { ITask } from './task.model';
 import { CreateTaskDto } from './create-task.dto';
+import { FindOneParams } from './find-one.params';
+import { UpdateTaskStatusDto } from './update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -20,18 +23,34 @@ export class TasksController {
   }
 
   @Get('/:id')
-  public findOne(@Param('id') id: string): ITask {
-    const task = this.tasksService.findOne(id);
-
-    if (task) {
-      return task;
-    }
-
-    throw new NotFoundException();
+  public findOne(@Param() params: FindOneParams): ITask {
+    return this.findOneOrFail(params.id);
   }
 
   @Post()
   public create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  public updateTaskStatus(
+    @Param() params: FindOneParams,
+    @Body() body: UpdateTaskStatusDto,
+  ): ITask {
+    const task = this.findOneOrFail(params.id);
+
+    task.status = body.status;
+
+    return task;
+  }
+
+  private findOneOrFail(id: string): ITask {
+    const task = this.tasksService.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException();
+    }
+
+    return task;
   }
 }
